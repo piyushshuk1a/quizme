704307cde5ef9d5cda3db7018433815dde30472c
piyushshuk1a <piyushshukla6396@gmail.com>
2025-09-07 19:09:29 +0530

    rbac based get-started section

--- frontend/src/containers/Home/GetStarted.tsx
+++ frontend/src/containers/Home/GetStarted.tsx
@@ -2,8 +2,29 @@ import { RocketLaunch } from '@mui/icons-material';
 import { Box, Typography } from '@mui/material';
 
 import { Button, Container } from '@/components';
+import { USER_ROLES } from '@/constants';
+import { useUserInfo } from '@/hooks';
 
 export const GetStarted = () => {
+  const { role = USER_ROLES.candidate } = useUserInfo();
+
+  const roleToTextMap = {
+    [USER_ROLES.candidate]: {
+      heading: 'Ready to Challenge Your Mind?',
+      subtitle:
+        'Join thousands of quiz enthusiasts and start your learning journey today.',
+      btnLabel: 'Start Your First Quiz',
+    },
+    [USER_ROLES.admin]: {
+      heading: 'Ready to get started?',
+      subtitle:
+        'Join thousands of educators, trainers, and knowledge enthusiasts who are already using our platform.',
+      btnLabel: 'Create Your First Quiz',
+    },
+  };
+
+  const { heading, subtitle, btnLabel } = roleToTextMap[role];
+
   return (
     <Box
       display="flex"
@@ -23,15 +44,12 @@ export const GetStarted = () => {
         gap={16}
       >
         <Typography component="h2" variant="h4">
-          Ready to Challenge Your Mind?
+          {heading}
         </Typography>
-        <Typography>
-          Join thousands of quiz enthusiasts and start your learning journey
-          today.
-        </Typography>
+        <Typography>{subtitle}</Typography>
         <Box display="flex" justifyContent="center" gap={12} marginTop={20}>
           <Button color="white" startIcon={<RocketLaunch />}>
-            Start Your First Quiz
+            {btnLabel}
           </Button>
         </Box>
       </Container>
